9b41345fa268f20ed292298f0987a30b83244d29
alexander.khavronin <alexander.khavronin@betera.by>
2026-01-25 16:40:09 +0300

    init: create project

---
+++ task1.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const name: string = "Александр";
+
+const text: string = `Я, ${name}, учу TYPESCRIPT`;
+
+//Написать программу, которая выводит text в нижнем регистре с вашим именем, если имя не совпадает, то заменить его
+
+const yourName: string = "Настя";
---
+++ task2.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const num1: number = 0.7767667999999;
+const num2: number = 3.2232332;
+
+//Написать программу, которая выведет целочисленную сумму двух чисел, для оптимизации - сделать проверку на целое число
+
+const newNum = num1 + num2;
