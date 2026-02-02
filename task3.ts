abe0737a2a65976ff684ace479ae6ca357d3cb48
sdajqgf-max <sdajqgf@gmail.com>
2026-02-02 05:31:56 -0800

    feat: task4 done

--- task3.ts
+++ task3.ts
@@ -1,6 +1,7 @@
 //Вывести сегодняшнюю дату в формате:
 //День/Месяц/Год Часы:Минуты:Секунду - Например 02/05/2026 22:15:30
 const now: Date = new Date();
+
 const day = now.getDate().toString().padStart(2, "0");
 const month = (now.getMonth() + 1).toString().padStart(2, "0");
 const year = now.getFullYear();
