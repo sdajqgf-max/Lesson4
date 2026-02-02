72c46fbebd0c8ba1524da158a2121bc12e70043b
sdajqgf-max <sdajqgf@gmail.com>
2026-02-02 03:13:19 -0800

    feat: add check for yourName in text

--- task1.ts
+++ task1.ts
@@ -1,8 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const name: string = "Александр";
 
 const text: string = `Я, ${name}, учу TYPESCRIPT`;
 
 //Написать программу, которая выводит text в нижнем регистре с вашим именем, если имя не совпадает, то заменить его
 
-const yourName: string = "Настя";
+const yourName: string = "Саша";
+
+if (text.includes(yourName)){
+    console.log(text.toLowerCase());
+}
